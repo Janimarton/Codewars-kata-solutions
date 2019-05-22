fca3a3db68ac0f98d37fe3b7abd56cea3172bcdf
János Márton <janimarton22@gmail.com>
2019-05-22 11:23:12 +0200

    basic tests for FoldArray created

--- src/array/FoldAnArray.java
+++ src/array/FoldAnArray.java
@@ -4,4 +4,8 @@ public class FoldAnArray {
 
     private final String kataLink = "https://www.codewars.com/kata/57ea70aa5500adfe8a000110";
 
+    public static int[] foldArray(int[] input, int runs) {
+        return new int[]{0};
+    }
+
 }
