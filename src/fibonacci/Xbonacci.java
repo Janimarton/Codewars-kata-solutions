fbb3a61cbbd6f80198fed055f439809056592f90
János Márton <janimarton22@gmail.com>
2019-05-27 13:31:11 +0200

    tribonacci 6kyu kata solved

---
+++ src/fibonacci/Xbonacci.java
@@ -0,0 +1,29 @@
+package fibonacci;
+
+public class Xbonacci {
+
+    private final String kataLink = "https://www.codewars.com/kata/556deca17c58da83c00002db/train/javascript";
+
+    public double[] tribonacci(double[] s, int n) {
+
+        if (n == 0) return new double[0];
+        double[] nums = new double[n];
+
+        if (n > 3) {
+            for (int i = 0; i < 3; i++) {
+                nums[i] = s[i];
+            }
+            for (int i = 3; i < n; i++) {
+                nums[i] = nums[i - 3] + nums[i - 2] + nums[i - 1];
+            }
+
+        } else {
+            for (int i = 0; i < n; i++) {
+                nums[i] = s[i];
+            }
+        }
+
+        return nums;
+    }
+
+}
